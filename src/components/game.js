cd2b6765041d0617bf51c02262835cd2cda32f14
Johnny Quinn <jftq97@gmail.com>
2022-01-14 15:11:26 -0500

    added gamePage counter to localstorage, successfully switched to other houses

--- src/components/game.js
+++ src/components/game.js
@@ -14,11 +14,27 @@ class Game extends React.Component {
             guessInput: '',
             guessFinal: '',
             guessDiff: 0,
-            price: Number(HomeData[0]["price"])
+            price: 0,
+            gamePage: Number(localStorage.getItem('gamePage')),
+            homePics: [],
+            location: '',
+            bed: '',
+            bath: '',
+            sq: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // sets the price variable equal to the price from homeData based on the current gamePage
+    setCurrData() {
+        this.state.price = HomeData[this.state.gamePage]["price"];
+        this.state.homePics = HomeData[this.state.gamePage]["homePics"]
+        this.state.location = HomeData[this.state.gamePage]['location']
+        this.state.bed = HomeData[this.state.gamePage]['bed']
+        this.state.bath = HomeData[this.state.gamePage]['bath']
+        this.state.sq = HomeData[this.state.gamePage]['sq']
     }
 
     handleChange(event) {    
@@ -27,11 +43,16 @@ class Game extends React.Component {
 
     handleSubmit() {
         this.state.guessFinal = Number(this.state.guessInput)
+        // compares user guess with actual price
         this.state.guessDiff = this.state.price - this.state.guessFinal
+        localStorage.setItem('gamePage', Number(this.state.gamePage + 1))
         alert(`The actual price was: $${this.state.price}\nYour guess: $${this.state.guessFinal}\nThe difference is: $${this.state.guessDiff}`)
     }
 
     render() {    
+        this.setCurrData()
+        console.log(`localStorage: ${localStorage.getItem('gamePage')}`)
+        console.log(`this.state.gamePage: ${this.state.gamePage}`)
         return (
             <div className="game">
                 <p>{this.state.other}</p>
@@ -46,33 +67,33 @@ class Game extends React.Component {
                                             <Col>
                                                 <Carousel>
                                                     <Carousel.Item>
-                                                        <img style={this.state.cardImgStyle} src={HomeData[0]["homePics"][0]}/>
+                                                        <img style={this.state.cardImgStyle} src={this.state.homePics[0]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][1]}/>
+                                                        <img src={this.state.homePics[1]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][2]}/>
+                                                        <img src={this.state.homePics[2]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][3]}/>
+                                                        <img src={this.state.homePics[3]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][4]}/>
+                                                        <img src={this.state.homePics[4]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][5]}/>
+                                                        <img src={this.state.homePics[5]}/>
                                                     </Carousel.Item>
                                                     <Carousel.Item>
-                                                        <img src={HomeData[0]["homePics"][6]}/>
+                                                        <img src={this.state.homePics[6]}/>
                                                     </Carousel.Item>
                                                 </Carousel>
                                             </Col>
                                             <Col>
-                                                <Card.Title><h1>Location: </h1>{HomeData[0]['location']}</Card.Title>
-                                                <Card.Text><h2>Bed(s):</h2> {HomeData[0]['bed']} </Card.Text>
-                                                <Card.Text><h2>Bath(s):</h2> {HomeData[0]['bath']}</Card.Text>
-                                                <Card.Text><h2>Square Footage:</h2> {HomeData[0]['sq']}</Card.Text>
+                                                <Card.Title><h1>Location: </h1>{this.state.location}</Card.Title>
+                                                <Card.Text><h2>Bed(s):</h2> {this.state.bed} </Card.Text>
+                                                <Card.Text><h2>Bath(s):</h2> {this.state.bath}</Card.Text>
+                                                <Card.Text><h2>Square Footage:</h2> {this.state.sq}</Card.Text>
                                             </Col>
                                         </Row>
                                     </Container>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 // import "./Landing.css"
 
 
-function Landing() {
-    return (
-        <div className="landing">
-            <h1>EstateGuesser</h1>
-            <h2>Can you guess the price of a <strong>random house?</strong></h2>
-            <button><Link to="/game"> Play </Link></button>
-        </div>
-    )
+class Landing extends React.Component { 
+    constructor() {
+        super()
+        this.state = {
+            /* when user loads up landing page, the gamePage is set to 0 
+               so the game is reset to the beginning
+            */
+            gamePage: 0, 
+        }
+    }
+    render() {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('gamePage', Number(this.state.gamePage))
+            console.log(`gamePage: ${window.localStorage.getItem('gamePage')}`);
+        }
+        return (
+            <div className="landing">
+                <h1>EstateGuesser</h1>
+                <h2>Can you guess the price of a <strong>random house?</strong></h2>
+                <Button variant="primary"><Link to="/game" style={{ color: "white" }}> PLAY </Link></Button>
+            </div>
+        )
+    }
 }
-
 export default Landing
