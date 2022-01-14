e9aab7bbda97ddb6a6b86d519c2be37872b8ad37
Johnny Quinn <jftq97@gmail.com>
2022-01-14 04:32:57 -0500

    added /home-data and successfully displayed image on game page

--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Carousel } from 'react-bootstrap';
+import HomeData from "../home-data/home-data-complete.json"
 
 function Game() {
     return (
@@ -10,6 +11,11 @@ function Game() {
                     <Col>
                         <Card border="primary">
                             <Card.Body>
+                                <Carousel>
+                                    <Carousel.Item>
+                                        <img src={HomeData[0]["homePics"][0]}/>
+                                    </Carousel.Item>
+                                </Carousel>
                                 <Card.Title>asdf</Card.Title>
                                 <Card.Text>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
