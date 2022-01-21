a34ed42cc3798c6c0c2d6c245a8d47d3090179e8
Johnny Quinn <jftq97@gmail.com>
2022-01-21 16:32:02 -0500

    got context hooks kind of working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { createContext ,useState } from 'react';
 
 import Landing from './components/landing'; 
 import Game from './components/game';
 
+
+export const GameContext = createContext()
+
 function App() {
+  const [gamePage, setGamePage] = useState(0)
+  const [houseIndexArr, setHouseIndexArr] = useState([])
   return (
-    <div className="App">
+    <div className="App"> 
         <Routes>
-          <Route exact path="/" element={<Landing />}/>
-          <Route path="/game" element={<Game />}/>
+          <Route exact path="/" element={
+            <GameContext.Provider value={[gamePage, setGamePage, houseIndexArr, setHouseIndexArr]}>
+              <Landing/>
+            </GameContext.Provider>}>
+          </Route>
+          
+          <Route path="/game" element={<Game/>}/>
         </Routes>
     </div>
   );
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,51 +1,42 @@
-import {Component} from "react";
+import {useContext} from "react";
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { GameContext } from "../App";
 // import "./Landing.css"
 
-class Landing extends Component { 
-    constructor() {
-        super()
-        this.state = {
-            /* when user loads up landing page, the gamePage is set to 0 
-               so the game is reset to the beginning
-            */
-            gamePage: 0, 
-            houseIndex: [],
-        }
-    }
-    shuffleHouseIndex() {
-        this.state.houseIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        let array = this.state.houseIndex
-        let currentIndex = array.length,  randomIndex;
+function Landing() { 
+    const gameContext = useContext(GameContext)
+    console.log(gameContext[0])
+    gameContext[1](3)
+    console.log(gameContext[0])
+    console.log(gameContext[2])
+
+    // function shuffleHouseIndex() {
+    //     this.state.houseIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    //     let array = this.state.houseIndex
+    //     let currentIndex = array.length,  randomIndex;
       
-        // While there remain elements to shuffle...
-        while (currentIndex != 0) {
+    //     // While there remain elements to shuffle...
+    //     while (currentIndex != 0) {
       
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
+    //       // Pick a remaining element...
+    //       randomIndex = Math.floor(Math.random() * currentIndex);
+    //       currentIndex--;
       
-          // And swap it with the current element.
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
+    //       // And swap it with the current element.
+    //       [array[currentIndex], array[randomIndex]] = [
+    //         array[randomIndex], array[currentIndex]];
+    //     }
       
-        this.state.houseIndex = array
-      }
-      
-    render() {
-            this.shuffleHouseIndex()
-            localStorage.setItem('gamePage', Number(this.state.gamePage))
-            localStorage.setItem('houseIndex', this.state.houseIndex)
-            console.log(`gamePage: ${window.localStorage.getItem('gamePage')}\nhouseIndex: ${this.state.houseIndex}`);
-            return (
+    //     this.state.houseIndex = array
+    // }
+    
+    return (
             <div className="landing">
                 <h1>EstateGuesser</h1>
                 <h2>Can you guess the price of a <strong>random house?</strong></h2>
                 <Button variant="primary"><Link to="/game" style={{ color: "white" }}> PLAY </Link></Button>
             </div>
-        )
-    }
+    )
 }
 export default Landing
