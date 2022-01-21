ead497003feba4cae18fc63750a80a6ce907291d
Johnny Quinn <jftq97@gmail.com>
2022-01-21 10:34:26 -0500

    Successfully scraped for 80 individual house links from 10 different cities, modulated scraper code

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -9,115 +9,36 @@ const colors = require('colors')
 
 const log = console.log;
 
-const headless = true;
+const headless = true; // headless: false means browser window opens, headless: true means without browser window 
 const args = [`--window-size=${1920},${1080}`]
 let browserRestarted = false;
 
 // stealth mode for the web scraper
 puppeteer.use(StealthPlugin());
 
-// runScraper()
+runScraper()
 
 async function runScraper() {
-        // initalize browser, headless: false means browser window opens, headless: true means without browser window 
-        let browser = await puppeteer.launch({ headless: headless, args: args});
-        let page = await browser.newPage();
-        log('> Browser intialized\n'.brightYellow)
-
-        // url for washington dc homes, price > $900k
-        const dc_url = 'https://www.zillow.com/washington-dc/houses/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Washington%2C%20DC%22%2C%22mapBounds%22%3A%7B%22west%22%3A-77.41626362202771%2C%22east%22%3A-76.61288837788709%2C%22south%22%3A38.677702084919346%2C%22north%22%3A39.108982541733354%7D%2C%22mapZoom%22%3A11%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A41568%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A900000%7D%2C%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22mf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22min%22%3A3035%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sort%22%3A%7B%22value%22%3A%22priced%22%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%2C%22apco%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D'
-        log('> Going to DC\n'.brightYellow)
-        await page.goto(dc_url);
-
-        // wait for specific page elements
-        await page.waitForSelector('li');
-        // set houses equal to specific DOM elements 
-        const houses = await page.$$('.list-card-info');
-
-        //list for hyperlinks to the more detailed page for each house
-        const houses_href_list = []
+        // get all the city URLS (with search params)
+        const cityURLs = await scrapeCityURLs()
         
-        log('> Pulling home listing links\n'.brightYellow)
+        // scrape each cities' page for URLs to house detail pages 
+        const house_URL_list = await goToEachCity(cityURLs)
+    
+        log(`> houses_URL_list.length: ${house_URL_list.length}`)
 
-        //breaking on 10 houses, JS doesnt load for the element unless scrolled down to 
-        //go through first 8 houses and retrieve url for the more detailed page for each house
-        for(let i = 0; i < 8; i++){
+        log(`> starting to scrape each individual house page\n`.yellow)
+        await goToEachHouse(house_URL_list)
 
-            const house = houses[i];
-
-            const househref = await house.$eval('a', e => e.getAttribute('href'))
-
-            houses_href_list.push(househref)
-        }
-        log(houses_href_list)
-
-        //inital json data for all data for writing to home-data file
-        let homesData = {}
-
-        // go to each page in house_href_list then retrieve and store info in JSON
-        for(let i = 0; i < houses_href_list.length; i++){
-            housePage = houses_href_list[i]
-
-            // gives time for browser to reintialize before continuing (if the browser was just restarted)
-            if(browserRestarted){
-                await page.waitForTimeout(1000).then(async () => {
-                    page = await browser.newPage();
-                    browserRestarted = false;
-                })
-            } 
-
-            await page.goto(housePage).catch(console.error)
-            log(`> At location[${i}]\n`.brightYellow)
-            
-            log('> Scanning for bot protection\n'.brightYellow)
-            // if bot protection page is detected then restart browser and go back a loop iteration 
-            const robotDetected = await robotDetect(page);
-            if(robotDetected){
-                //for some reason I cannot isolate this restarting browser sequence in a function thus cannot keep DRY
-                log('> Restarting browser instance\n'.brightYellow)
-                browserRestarted = true;
-                await browser.close()
-                browser = await puppeteer.launch({ headless: headless, args: args});
-                i -= 1; 
-                continue;
-            }
-
-            // sometimes the only thing that loads is an ad (I think that has something to do with the bot protection)
-            // so if the page didn't load as expected (with <title>), restart the browser and go back a loop iteration 
-            const titleLoaded = await checkTitleLoaded(page);
-            if(!titleLoaded){
-                log('> Restarting browser instance\n'.brightYellow)
-                browserRestarted = true;
-                await browser.close()
-                browser =  await puppeteer.launch({ headless: headless, args: args});
-                i -= 1; 
-                continue;
-            }
-
-            // if no bot protection and the correct page loaded then continue on with scraping for data
-            homesData[i] = await scrapeHomeDetailPages(page)
-            log('--------------------------------------------------------------------------------------')
-        }
-        // for testing
-        // const testurl = 'https://www.zillow.com/homede tails/3122-3124-P-St-NW-Washington-DC-20007/35725211_zpid/'
-        // await page.goto(testurl);
-
-        // formats json data
-        homesData = JSON.stringify(homesData, null, 2);
-        //write json data to file
-        fs.writeFileSync('home-data.json', homesData)
-
-        await browser.close()
-
-        log('SUCCESS!'.rainbow)
+        log('\nSUCCESS!\n'.rainbow)
 }
 
 /* 
     loops through hardcoded list of cities, goes to their individual pages, filter results, 
-    and grabs the resulting URL (which includes the filter params)
+    and grabs the resulting URL (which includes the search filter params)
 */
-async function getCityURLS() {
-    let browser = await puppeteer.launch({ headless: true, args: args});
+async function scrapeCityURLs() {
+    let browser = await puppeteer.launch({ headless: headless, args: args});
     let page = await browser.newPage();
     log('> Browser intialized\n'.brightYellow)
 
@@ -171,7 +92,8 @@ async function getCityURLS() {
         await page.waitForSelector('[data-value="priced"]').then(async () => {
             log('> [data-value=priced] loaded'.green)
             await page.click('[data-value="priced"]').then(() => {
-                log('> [data-value="priced"]'.green)
+                log('> [data-value="priced"]\n'.green)
+                log('--------------------------------------------------------------------------------------')
             }).catch(console.error)
         })
 
@@ -181,6 +103,145 @@ async function getCityURLS() {
     }
 
     log(cityURLs)
+
+    log(`> Browser closed`.brightYellow)
+    await browser.close()
+
+    return cityURLs
+}
+
+// goes to each individual city and runs scrapeHouseURls
+async function goToEachCity(cityURLs) {
+    let house_URL_list = []
+    
+    // initalize browser
+    let browser = await puppeteer.launch({ headless: headless, args: args});
+    let page = await browser.newPage();
+    log('> Browser intialized\n'.brightYellow)
+
+    log('> Scraping for house URLs within each city'.brightYellow)
+    for(let i=0;i<cityURLs.length;i++){
+        // gives time for browser to reintialize before continuing (if the browser was just restarted)
+        if(browserRestarted){
+            await page.waitForTimeout(1000).then(async () => {
+                page = await browser.newPage();
+                browserRestarted = false;
+            })
+        }
+
+        log(`> houseURL[${i}]`.brightYellow)
+        await page.goto(cityURLs[i])
+
+        const robotDetected = await robotDetect(page);
+        if(robotDetected){
+            //for some reason I cannot isolate this restarting browser sequence in a function thus cannot keep DRY
+            log('> Restarting browser instance\n'.brightYellow)
+            browserRestarted = true;
+            await browser.close()
+            browser = await puppeteer.launch({ headless: headless, args: args});
+            i -= 1; 
+            continue;
+        }
+
+        //scrape page for house URLS
+        let houseURLS = await scrapeHouseURLs(page)
+
+        house_URL_list.push.apply(house_URL_list, houseURLS)
+    }
+
+    log(`> Browser closed`.brightYellow)
+    await browser.close()
+
+    return house_URL_list
+}
+
+// scrapes for URLs to individual house detail pages 
+// TODO: add functionality to scroll page a little and load more of the content
+async function scrapeHouseURLs(page) {
+    await page.waitForSelector('li');
+    // set houses equal to specific DOM elements 
+    const houses = await page.$$('.list-card-info');
+
+    //list for hyperlinks to the more detailed page for each house
+    const houses_href_list = []
+    
+    log('> Pulling home listing links\n'.brightYellow)
+
+    //go through 8 houses and retrieve url for the more detailed page for each house
+    for(let i = 0; i < 8; i++){
+
+        const house = houses[i];
+
+        const househref = await house.$eval('a', e => e.getAttribute('href'))
+
+        houses_href_list.push(househref)
+    }
+    log(houses_href_list)
+
+    return houses_href_list
+}
+
+async function goToEachHouse(houses_URL_list){
+    // initalize browser
+    let browser = await puppeteer.launch({ headless: headless, args: args});
+    let page = await browser.newPage();
+    log('> Browser intialized\n'.brightYellow)
+
+    //inital json data for all data for writing to home-data file
+    let homesData = {}
+
+    // go to each page in house_URL_list then retrieve and store info in JSON
+    for(let i = 0; i < houses_URL_list.length; i++){
+        housePage = houses_URL_list[i]
+
+        // gives time for browser to reintialize before continuing (if the browser was just restarted)
+        if(browserRestarted){
+            await page.waitForTimeout(1000).then(async () => {
+                page = await browser.newPage();
+                browserRestarted = false;
+            })
+        } 
+
+        await page.goto(housePage).catch(console.error)
+        log(`> At location[${i}]\n`.brightYellow)
+        
+        log('> Scanning for bot protection\n'.brightYellow)
+        // if bot protection page is detected then restart browser and go back a loop iteration 
+        const robotDetected = await robotDetect(page);
+        if(robotDetected){
+            //for some reason I cannot isolate this restarting browser sequence in a function thus cannot keep DRY
+            log('> Restarting browser instance\n'.brightYellow)
+            browserRestarted = true;
+            await browser.close()
+            browser = await puppeteer.launch({ headless: headless, args: args});
+            i -= 1; 
+            continue;
+        }
+
+        // sometimes the only thing that loads is an ad (I think that has something to do with the bot protection)
+        // so if the page didn't load as expected (with <title>), restart the browser and go back a loop iteration 
+        const titleLoaded = await checkTitleLoaded(page);
+        if(!titleLoaded){
+            log('> Restarting browser instance\n'.brightYellow)
+            browserRestarted = true;
+            await browser.close()
+            browser =  await puppeteer.launch({ headless: headless, args: args});
+            i -= 1; 
+            continue;
+        }
+
+        // if no bot protection and the correct page loaded then continue on with scraping for data
+        homesData[i] = await scrapeHomeDetailPages(page)
+        log('--------------------------------------------------------------------------------------')
+    }
+    // for testing
+    // const testurl = 'https://www.zillow.com/homede tails/3122-3124-P-St-NW-Washington-DC-20007/35725211_zpid/'
+    // await page.goto(testurl);
+
+    // formats json data
+    homesData = JSON.stringify(homesData, null, 2);
+    //write json data to file
+    fs.writeFileSync('home-data.json', homesData)
 
     await browser.close()
 }
